f557ba3b5e5a25dcb1bf9c56a6998301ac7fdb35
Gaurav Kumar <gauravlodhi530@gmail.com>
2025-11-20 11:43:16 +0530

    change backend url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,3 @@
-// export const API_URL = "https://zenoai-uy9e.onrender.com";
-export const API_URL = "http://localhost:3000";
-export const SOCKET_URL = "http://localhost:3000";
-// export const SOCKET_URL = "https://zenoai-uy9e.onrender.com";
+export const API_URL = "https://zenoai-uy9e.onrender.com";
+
+export const SOCKET_URL = "https://zenoai-uy9e.onrender.com";
