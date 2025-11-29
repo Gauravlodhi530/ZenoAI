e1b7704f6629d8c677ee1e513a72b1f31bff04f1
Gaurav Kumar <gauravlodhi530@gmail.com>
2025-11-29 14:07:32 +0530

    change api url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 
-export const API_URL = "http://localhost:3000";
-// export const API_URL = "https://zenoai-uy9e.onrender.com";
+// export const API_URL = "http://localhost:3000";
+export const API_URL = "https://zenoai-uy9e.onrender.com";
 
-export const SOCKET_URL = "http://localhost:3000";
-// export const SOCKET_URL = "https://zenoai-uy9e.onrender.com";
+// export const SOCKET_URL = "http://localhost:3000";
+export const SOCKET_URL = "https://zenoai-uy9e.onrender.com";
 
 // Create axios instance with credentials enabled
 export const apiClient = axios.create({
